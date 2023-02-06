19e9889c94db74749e39a10e7f981b8b3726c53f
Vilhelm Prytz <vilhelm@prytznet.se>
2023-02-06 15:55:24 +0100

    initial work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Ok, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// Database operation
+    operation: String,
+    /// The query to execute
+    query: String,
+}
+
+#[derive(Debug)]
+enum Operation {
+    Insert,
+    Delete,
+    SelectAll,
+    Get,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    // map the operation to an enum
+    let operation = match args.operation.as_str() {
+        "insert" => Operation::Insert,
+        "delete" => Operation::Delete,
+        "select_all" => Operation::SelectAll,
+        "get" => Operation::Get,
+        _ => panic!("Operation not supported"),
+    };
+
+    println!("Operation: {:?}", operation);
+    println!("Query: {}", args.query);
+
+    Ok(())
 }
